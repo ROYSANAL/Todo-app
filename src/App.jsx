af326180f2ebbcc3b04688c5b0f9053d4e967b74
ROYSANAL <sanalroy12345@gmail.com>
2025-01-22 15:14:47 +0530

    Modified index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { TodoProvider } from "./contexts";
-import { stringify } from "postcss";
 import { TodoForm, TodoItem } from "./components";
 
 function App() {
